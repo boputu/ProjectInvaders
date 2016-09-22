44339202cf77a0bc0d604b2f474026a890ac72ca
Borja Pulido Tudela <boputu@floridauniversitaria.es>
2016-09-22 09:58:41 +0200

    ejercicio 3 php

---
+++ indexmodify.php
@@ -0,0 +1,17 @@
+	<?php
+//fichero index.php
+//creado por Paco Gomez
+	?>
+<html>
+  <head>
+      <title>ACTIVIDAD 03</title>
+  </head>
+  <body>
+      <?php
+        echo "Mi nombre es Borja<br>";
+        echo "Mi primer apellido es Pulido<br>";
+        print "Y mi segundo apellido es Tudela<br>";
+      ?>
+  </body>
+</html>
+
