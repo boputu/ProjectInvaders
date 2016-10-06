ae2f463a9b78075b47f7c16c16ae2a5f08f450b3
Borja Pulido Tudela <boputu@floridauniversitaria.es>
2016-10-06 11:37:56 +0200

    Fase 2 Pocket Invaders (Haciendo Click)

---
+++ pInvadersF2-master/lib/ia.php
@@ -0,0 +1,33 @@
+<?php
+
+class ia
+{
+
+  //PROPIEDADES
+
+  public $fila = '3';
+  public $columna = '1';
+
+  //MÉTODOS
+  public function getfila() {
+    return $this->fila;
+  }
+  public function getcolumna() {
+    return $this->columna;
+  }
+  //SETTERS
+  public function setfila($fil) {
+    $this->fila = $fil;
+  }
+  public function setcolumna($col) {
+    $this->columna = $col;
+  }
+
+  //RANDOM
+public function RandomPos() {
+
+
+  }
+}
+
+?>
